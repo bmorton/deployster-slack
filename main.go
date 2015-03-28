69f783a08063306408a8c5c1920caf7b489589ba
Brian Morton <brian@mmm.hm>
2015-03-28 03:07:30 -0700

    Implement Slack library for delivering message events to a Slack channel.

--- main.go
+++ main.go
@@ -2,22 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/coreos/go-etcd/etcd"
+	"github.com/nlopes/slack"
 )
 
 func main() {
-	client := etcd.NewClient([]string{"http://127.0.0.1:2379"})
+	client := etcd.NewClient([]string{os.Getenv("ETCDCTL_PEERS")})
 	plugin := New("slack", client)
 
-	plugin.SetHandler(&EventHandler{})
+	api := slack.New(os.Getenv("SLACK_TOKEN"))
+	plugin.SetDeployHandler(&DeployEventHandler{client: api, channel: os.Getenv("SLACK_CHANNEL")})
 	plugin.ReplayUnseen = true
 
-	plugin.Run()
+	log.Println(plugin.Run())
 }
 
-type EventHandler struct{}
+type DeployEventHandler struct {
+	client  *slack.Slack
+	channel string
+}
 
-func (e *EventHandler) Handle(event string) {
-	fmt.Println(event)
+func (e *DeployEventHandler) Handle(event *DeployEvent) {
+	params := slack.PostMessageParameters{
+		Username: "Deployster",
+	}
+	message := fmt.Sprintf("Deploy started for %d instance(s) of %s (version: %s) at %s", event.InstanceCount, event.ServiceName, event.Version, event.Timestamp)
+	channel, timestamp, err := e.client.PostMessage(e.channel, message, params)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+
+	log.Printf("Message successfully sent to channel %s at %s", channel, timestamp)
 }
